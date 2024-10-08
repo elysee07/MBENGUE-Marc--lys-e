cc5d921e9607ac953070320e18ab050c89240643
Daniel <daniel@MacBook-Air-de-Daniel.local>
2024-10-08 16:06:44 +0100

    Sauvergarde de la page

---
+++ Js/srcipt.js
@@ -0,0 +1,8 @@
+/*====================== typing animation ===================*/
+var typed = new Typed(".typing",
+{
+    strings:["Référent Digital", "UI/UX Designer", "Marketeur Web", "Community Manager"],
+    typeSpeed:100,
+    BacSpeed:60,
+    loop:true
+})
